79eea3957379d446c032dcb6b0714c7d536552b0
wangyufei993 <1531481086@qq.com>
2016-03-17 11:34:25 +0800

    jisuan
    …

--- testadd/ViewController.swift
+++ testadd/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBAction func add(sender: UIButton) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!x.text!.isEmpty){
+        a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty){
+            b=(Double)(y.text!)
+        }
+        c=a+b
+        z.text="\(c)"
+
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
