e4b602d62b7533fba95ec51ceef537a93911f21d
wangyufei993 <1531481086@qq.com>
2016-03-31 11:36:30 +0800

    xiugai
    加入新的绑定和代码

--- testadd/ViewController.swift
+++ testadd/ViewController.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var x: UITextField!
-    @IBOutlet weak var z: UITextField!
-    @IBOutlet weak var y: UITextField!
-    @IBAction func add(sender: UIButton) {
-        var a:Double!=0
-        var b:Double!=0
-        var c:Double!=0
-        if(!x.text!.isEmpty){
-        a=(x.text! as NSString).doubleValue
-        }
-        if(!y.text!.isEmpty){
-            b=(Double)(y.text!)
-        }
-        c=a+b
-        z.text="\(c)"
-
-        
-    }
-    @IBOutlet weak var 文本框: UITextField!
+    var 计算器:计算 = 计算()
+    var 显示缓存:String = ""
+    var 缓存 = 0.0
+    var 缓存2:Double?
+    
+    @IBOutlet weak var 屏幕: UITextField!
     @IBAction func 数字1(sender: AnyObject) {
+        点按("1")
+
     }
     @IBAction func 数字2(sender: AnyObject) {
     }
@@ -49,11 +37,62 @@ class ViewController: UIViewController {
     }
     @IBAction func 数字0(sender: AnyObject) {
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func 算法加(sender: AnyObject) {
+    }
+    @IBAction func 算法减(sender: AnyObject) {
     }
    
+    @IBAction func 算法乘(sender: AnyObject) {
+    }
+    @IBAction func 算法除(sender: AnyObject) {
+    }
+    @IBAction func 运算结果(sender: AnyObject) {
+    }
+  
+    @IBAction func 数字点(sender: AnyObject) {
+    }
+    func 点按(数字:String) {
+        显示缓存 += 数字
+        self.屏幕.text = 显示缓存
+    }
+    func 求值() {
+        
+        if !显示缓存.isEmpty {
+            let 临时 = 显示缓存 as NSString
+            缓存 = 临时.doubleValue
+            显示缓存 = ""
+        }
+        let 临时 = 缓存
+        var 结果 = ""
+        
+        if let 前一个数字 = 缓存2 {
+            结果 = 计算器.求结果(前一个数字, 被操作数: 临时)
+            
+            self.屏幕.text = 结果
+            let 临 = 结果 as NSString
+            缓存2 = 临.doubleValue
+        } else {
+            缓存2 = 缓存
+            缓存 = 0.0
+        }
+        
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        屏幕.editing
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+   // override func didReceiveMemoryWarning() {
+   //     super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    //}
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
